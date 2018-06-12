75124fec3d6b509e8211623a6f16ef5ddd9d689e
Areeb Ahmed <areebahmed916@gmail.com>
2018-06-11 19:10:16 -0500

    added alpha beta pruning

--- tictactoe.h
+++ tictactoe.h
@@ -15,7 +15,7 @@ void makeMove(const char board[3][3], int & row, int & col);
 void showBoard(const char board[3][3]);
 char winner(const char board[3][3]);
 char otherMark(char mark);
-int minimax(char board[3][3], const char mark, const int turns, const bool max);
+int minimax(char board[3][3], const char mark, const int turns, const bool max, int alpha, int beta);
 void computer(char board[3][3], const char mark);
 
 #endif
